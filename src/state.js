a870094ec319e6a0a7612911c8f512b36c145a76
Dmitriy <42001369+Hromenkov@users.noreply.github.com>
2025-08-21 22:17:37 +0300

    Create state.js

---
+++ src/state.js
@@ -0,0 +1,8 @@
+export const state = {
+  screen: 'home',
+  currentBook: 'mrk',      // старт: Евангелие от Марка
+  currentChapter: 1,
+  currentTranslation: 'ru_synodal',
+  currentLanguage: 'ru',
+  bookmarks: new Set(),
+};
