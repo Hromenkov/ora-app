102c5148eee4cacb7069205e2ac1c6d8971b693a
Dmitriy <42001369+Hromenkov@users.noreply.github.com>
2025-08-22 17:21:15 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 // src/main.js
-import { renderHome } from './ui/home.js?v=boot9';
-import { initRouter } from './router.js?v=boot9';
+import { renderHome } from './ui/home.js?v=boot10';
+import { initRouter } from './router.js?v=boot10';
 
 (function initTelegram(){
   if (window.Telegram?.WebApp) {
-    const tg = window.Telegram.WebApp;
-    try { tg.ready(); tg.expand(); } catch {}
+    try { Telegram.WebApp.ready(); Telegram.WebApp.expand(); } catch {}
   }
 })();
 
